f508888904b62d09deebbb226400811048a37d7d
Gabe Susman <gabe.susman@gmail.com>
2024-10-27 00:11:28 -0600

    first comit

---
+++ cat-cam-egress/cat_cam_egressApp.swift
@@ -0,0 +1,17 @@
+//
+//  cat_cam_egressApp.swift
+//  cat-cam-egress
+//
+//  Created by Max Gabriel Susman on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct cat_cam_egressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cat-cam-egressTests/cat_cam_egressTests.swift
@@ -0,0 +1,17 @@
+//
+//  cat_cam_egressTests.swift
+//  cat-cam-egressTests
+//
+//  Created by Max Gabriel Susman on 10/26/24.
+//
+
+import Testing
+@testable import cat_cam_egress
+
+struct cat_cam_egressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
